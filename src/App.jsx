eb326429ff3e7efaf16ca7419b00eb078b839f1b
Sajeel Ahmed Siddiqui <sajeel.ahmed@fmsl.local>
2022-06-17 20:07:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import NavBar from './components/navbar';
+import Footer from './components/footer';
+import Hero from './components/hero';
+import Services from './components/services';
+import Contact from './components/contact';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+      <Services/>
+      <Contact/>
+      <Footer/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+        <header className="text-gray-400 bg-gray-900 body-font">
+  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+      <svg  fill="none" stroke="currentColor"   className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
+        <path  d="M16.803,18.615h-4.535c-1,0-1.814-0.812-1.814-1.812v-4.535c0-1.002,0.814-1.814,1.814-1.814h4.535c1.001,0,1.813,0.812,1.813,1.814v4.535C18.616,17.803,17.804,18.615,16.803,18.615zM17.71,12.268c0-0.502-0.405-0.906-0.907-0.906h-4.535c-0.501,0-0.906,0.404-0.906,0.906v4.535c0,0.502,0.405,0.906,0.906,0.906h4.535c0.502,0,0.907-0.404,0.907-0.906V12.268z M16.803,9.546h-4.535c-1,0-1.814-0.812-1.814-1.814V3.198c0-1.002,0.814-1.814,1.814-1.814h4.535c1.001,0,1.813,0.812,1.813,1.814v4.534C18.616,8.734,17.804,9.546,16.803,9.546zM17.71,3.198c0-0.501-0.405-0.907-0.907-0.907h-4.535c-0.501,0-0.906,0.406-0.906,0.907v4.534c0,0.501,0.405,0.908,0.906,0.908h4.535c0.502,0,0.907-0.406,0.907-0.908V3.198z M7.733,18.615H3.198c-1.002,0-1.814-0.812-1.814-1.812v-4.535c0-1.002,0.812-1.814,1.814-1.814h4.535c1.002,0,1.814,0.812,1.814,1.814v4.535C9.547,17.803,8.735,18.615,7.733,18.615zM8.64,12.268c0-0.502-0.406-0.906-0.907-0.906H3.198c-0.501,0-0.907,0.404-0.907,0.906v4.535c0,0.502,0.406,0.906,0.907,0.906h4.535c0.501,0,0.907-0.404,0.907-0.906V12.268z M7.733,9.546H3.198c-1.002,0-1.814-0.812-1.814-1.814V3.198c0-1.002,0.812-1.814,1.814-1.814h4.535c1.002,0,1.814,0.812,1.814,1.814v4.534C9.547,8.734,8.735,9.546,7.733,9.546z M8.64,3.198c0-0.501-0.406-0.907-0.907-0.907H3.198c-0.501,0-0.907,0.406-0.907,0.907v4.534c0,0.501,0.406,0.908,0.907,0.908h4.535c0.501,0,0.907-0.406,0.907-0.908V3.198z"></path>
+      </svg>
+      <span className="ml-3 text-xl">The Novus Tech</span>
+    </a>
+    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-yellow-500	flex flex-wrap items-center text-base justify-center">
+      <a className="mr-5 hover:text-yellow-500">Home</a>
+      <a className="mr-5 hover:text-yellow-500">About</a>
+      <a className="mr-5 hover:text-yellow-500">Services</a>
+      <a className="mr-5 hover:text-yellow-500">Contact</a>
+    </nav>
+    <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-yellow-500 rounded text-white mt-4 md:mt-0">Hire Us!
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+    </div>
+  )
+}
